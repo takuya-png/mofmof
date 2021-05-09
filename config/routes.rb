af17443e5e50dbb435c79e8d5523028ad846ec5d
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-10 00:22:27 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'estates/new'
-  get 'estates/show'
-  get 'estates/edit'
-  get 'estates/index'
-  get 'estates/create'
-  get 'estates/update'
-  get 'estates/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'estates#index'
+  resources :estates
 end
